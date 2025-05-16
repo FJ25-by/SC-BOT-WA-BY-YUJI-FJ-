e4997da0656fd5f95c2ef03ad13c4bdc9583da69
FJ25-by <neksami2022@gmail.com>
2025-05-16 10:19:15 +0700

    first commit

---
+++ data/security/adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'approval': {
+    'num': '6282171316630',
+    'text': "setujui",
+    'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+    'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
+  },
+  'creatorScript': '6282171316630',
+  'filePath': "./data/approval",
+  'checkFilePath': "./rafatharcode.js",
+  'codeToDetect': 'connectToWhatsApp();'
+};
